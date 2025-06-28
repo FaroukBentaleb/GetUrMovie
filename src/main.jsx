34f5ee34352de0ef3383b23724c01bbd66d754f7
FaroukBentaleb <farouk.bentaleb@esprit.tn>
2025-06-28 16:44:07 +0100

    Changes made

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { Trend } from './components/Trending/Trend.jsx'
 import { Popular } from './components/Popular/Popular.jsx'
 import { Show } from './components/ShowAll/Show.jsx'
 import { Footer } from './components/Footer/Footer.jsx'
+console.log("✅ VITE_TMDB_API_KEY:", import.meta.env.VITE_TMDB_API_KEY);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header/>
